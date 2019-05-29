873afd283514e3b1985eafef7c5beb6f928da011
kimhyewon <gpdnjs8151@namver.com>
2019-05-29 11:36:22 +0900

    mysql

--- api.py
+++ api.py
@@ -1,7 +1,82 @@
-# from flask import Flask
-# from flask_restful import Resource, Api
-# from flask_restful import reqparse
-# from flaskext.mysql import MySQL
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from flaskext.mysql import MySQL
+
+# konlpy
+from collections import Counter
+from konlpy.tag import Twitter
+
+
+app = Flask(__name__)
+
+# MySQL 연결
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'bituser'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'bituser'
+app.config['MYSQL_DATABASE_DB'] = 'bit_project'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/getSongs')
+def get_songs():
+    return 'Hello World!'
+
+    
+@app.route('/test')
+def test():
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('sotryNo', type=int)
+        parser.add_argument('title', type=str)
+        parser.add_argument('storyContent', type=str)
+        args = parser.parse_args()
+
+        _sotryNo = args['sotryNo']
+        _title = args['title']
+        _storyContent = args['storyContent']
+
+        nlp = Twitter() 
+        nouns = nlp.nouns(_storyContent)
+        count = Counter(nouns)
+
+        result = []
+
+        for letter, i in count.most_common(5):
+            result.append(letter)
+
+
+        # TODO: 노래 추천 알고리즘
+        songid1 = 1
+
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sql = 'update story set songid1 = %s where storyno = %s'
+        cursor.execute(sql, (str(songid1), str(_sotryNo)))
+        data = cursor.fetchall()
+        conn.commit()
+
+        return "test"
+
+        # if len(data) is 0:
+        #     conn.commit()
+        #     return {'StatusCode': '200', 'Message': 'User creation success'}
+        # else:
+        #     return {'StatusCode': '1000', 'Message': str(data[0])}
+
+    except Exception as e:
+        return {'error': str(e)}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 # app = Flask(__name__)
 # api = Api(app)
@@ -100,13 +175,3 @@
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0')
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5009)
