648dab9875c01db0a1233c163a5ea2b4091a9cfe
kimhyewon <gpdnjs8151@namver.com>
2019-05-30 15:36:38 +0900

    flask complete

--- api.py
+++ api.py
@@ -7,6 +7,9 @@ from flaskext.mysql import MySQL
 from collections import Counter
 from konlpy.tag import Twitter
 
+# logging
+import logging
+
 
 app = Flask(__name__)
 
@@ -17,7 +20,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'bituser'
 app.config['MYSQL_DATABASE_DB'] = 'bit_project'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
 
 @app.route('/')
 def hello_world():
@@ -31,8 +33,9 @@ def get_songs():
 @app.route('/test')
 def test():
     try:
+        # JSON parameter 
         parser = reqparse.RequestParser()
-        parser.add_argument('sotryNo', type=int)
+        parser.add_argument('sotryNo', type=str)
         parser.add_argument('title', type=str)
         parser.add_argument('storyContent', type=str)
         args = parser.parse_args()
@@ -40,138 +43,87 @@ def test():
         _sotryNo = args['sotryNo']
         _title = args['title']
         _storyContent = args['storyContent']
-
+        
+        # 사연 fre1~5 변수 얻기
         nlp = Twitter() 
         nouns = nlp.nouns(_storyContent)
         count = Counter(nouns)
 
-        result = []
+        result = [] 
 
+        # 사연 fre1~5 result 배열에 저장
         for letter, i in count.most_common(5):
-            result.append(letter)
+            result.append(str(letter))
+            # logging.warning("type: ", type(letter))
+        
+
+        # logging.warning("hyewon result: ", result, type(result[0]))
 
 
-        # TODO: 노래 추천 알고리즘
-        songid1 = 1
-
-
+        # DB 설정1
         conn = mysql.connect()
         cursor = conn.cursor()
-        sql = 'update story set songid1 = %s where storyno = %s'
-        cursor.execute(sql, (str(songid1), str(_sotryNo)))
+
+        # TODO: 노래 추천 알고리즘
+
+        # 사연 fre1~5 가 속한 노래DB 데이터 얻기
+        sql = ' select songid, fre1, fre2, fre3, fre4, fre5 from song WHERE (FRE5 or FRE4 or FRE3 or FRE2 or FRE1) IN (%s, %s, %s, %s, %s)'
+        cursor.execute(sql, (result[0],result[1],result[2],result[3],result[4]))
+        rows = cursor.fetchall()
+            
+        # logging.warning('rows: ', rows) #logging
+
+
+
+        newRows = []
+
+
+        for row in rows:
+            l = list(row)
+            l.append(0)
+            newRows.append(l)
+
+        # logging.warning(newRows)
+       
+        for row in newRows:
+            for i in range(1,6):
+                if result[0] == row[i]:
+                    row[-1] += ((6-i) * 4)
+                if result[1] == row[i]:
+                    row[-1] += ((6-i) * 4)
+                if result[2] == row[i]:
+                    row[-1] += ((6-i) * 4)
+                if result[3] == row[i]:
+                    row[-1] += ((6-i) * 4)
+                if result[4] == row[i]:
+                    row[-1] += ((6-i) * 4)
+        # logging.warning("newRows: ", newRows)      
+
+        # 노래 3곡 songid get
+        newRows.sort(key=lambda x:x[6] , reverse=True)
+          
+        for i in range(0,3):
+            print("song_id :",newRows[i][0], "score : ", newRows[i][6])
+        
+        a="A"
+        
+
+        #업데이트
+        # conn = mysql.connect()
+        # cursor = conn.cursor()
+        sql = 'update story set songid1 = %s, songid2 = %s, songid3 = %s where storyno = %s'
+        cursor.execute(sql, (newRows[0][0],newRows[1][0],newRows[2][0], str(_sotryNo)))
         data = cursor.fetchall()
         conn.commit()
 
-        return "test"
-
-        # if len(data) is 0:
-        #     conn.commit()
-        #     return {'StatusCode': '200', 'Message': 'User creation success'}
-        # else:
-        #     return {'StatusCode': '1000', 'Message': str(data[0])}
+        if len(data) is 0:
+            conn.commit()
+            return {'StatusCode': '200', 'Message': 'User creation success'}
+        else:
+            return {'StatusCode': '1000', 'Message': str(data[0])}
 
     except Exception as e:
         return {'error': str(e)}
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# # MySQL 연결
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_USER'] = 'bituser'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'bituser'
-# app.config['MYSQL_DATABASE_DB'] = 'bamboo'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
-
-# class CreateUser(Resource):
-#     def post(self):
-#         try:
-#             parser = reqparse.RequestParser()
-#             parser.add_argument('email', type=str)
-#             parser.add_argument('user_name', type=str)
-#             parser.add_argument('password', type=str)
-#             args = parser.parse_args()
-
-#             _userEmail = args['email']
-#             _userName = args['user_name']
-#             _userPassword = args['password']
-
-#             conn = mysql.connect()
-#             cursor = conn.cursor()
-#             cursor.callproc('sp_create_user', (_userEmail, _userName, _userPassword))
-#             data = cursor.fetchall()
-
-#             if len(data) is 0:
-#                 conn.commit()
-#                 return {'StatusCode': '200', 'Message': 'User creation success'}
-#             else:
-#                 return {'StatusCode': '1000', 'Message': str(data[0])}
-#         except Exception as e:
-#             return {'error': str(e)}
-
-
-
-
-
-# # class setStory(Resource):
-# #     def post(self):
-# #         try:
-# #             parser = reqparse.RequestParser()
-# #             # TODO: storyNO
-# #             parser.add_argument('storyCONTENT', type=str)
-# #             # TODO: REGDATE
-# #             parser.add_argument('USERID', type=str)
-# #             # TODO: parser.add_argument('SONGID1', type=str)
-# #             # TODO: parser.add_argument('SONGID2', type=str)
-# #             # TODO: parser.add_argument('SONGID3', type=str)
-# #             parser.add_argument('TITLE', type=str)
-
-# #             args = parser.parse_args()
-
-# #             # _storyNO = args['storyNO']
-# #             _sotryContent = args['storyCONTENT']
-# #             # _REGDATE = args['REGDATE']
-# #             _USERID = args['USERID']
-# #             # _SONGID1 = args['SONGID1']
-# #             # _SONGID2 = args['SONGID2']
-# #             # _SONGID3 = args['SONGID3']
-# #             _TITLE = args['TITLE']
- 
-# class index(Resource):
-#     def hello_world():
-#         return 'Hello World!'
- 
-
-
-# #             conn = mysql.connect()
-# #             cursor = conn.cursor()
-# #             # TODO: create stored procedures 
-# #             # cursor.callproc('sp_create_user', (_userEmail, _userName, _userPassword))
-# #             data = cursor.fetchall()
-
-# #             if len(data) is 0:
-# #                 conn.commit()
-# #                 return {'StatusCode': '200', 'Message': 'story creation success'}
-# #             else:
-# #                 return {'StatusCode': '1000', 'Message': str(data[0])}
-# #         except Exception as e:
-# #             return {'error': str(e)}
-
-
-
-
-
-
-# api.add_resource(CreateUser, '/user')
-
-# api.add_resource(index, '/')
-# # api.add_resource(setStory, '/setStory')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
